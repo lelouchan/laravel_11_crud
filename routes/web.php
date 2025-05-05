79f90f3d3b8124f843de2b154b58a5288969d71d
root <hello@example.com>
2025-05-05 21:09:31 +0800

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -6,17 +6,17 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
-// Authentication Routes
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Registration Routes
+
 Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('/register', [RegisterController::class, 'register']);
 
-// Product Routes
+
 Route::resource('products', ProductController::class);
